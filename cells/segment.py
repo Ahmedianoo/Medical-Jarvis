78baa25a173d6240939f9bdf2e87cf4424d6eb29
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2025-11-27 15:35:25 +0200

    created thresholding mask for RBC

--- cells/segment.py
+++ cells/segment.py
@@ -42,6 +42,9 @@ NOTES:
 - Output masks will be used for counting and feature extraction in the next steps.
 
 
+color picker may be useful for threshold selection: https://redketchup.io/color-picker
+hue colors in opencv range from 0-179 as it uses 8 bits representation. 
+ - so if you selected a color in color selector divide the hue value by 2 to get the corresponding opencv hue value.
 """
 import cv2
 import numpy as np
@@ -49,3 +52,33 @@ from skimage import exposure
 from matplotlib import pyplot as plt
 import os
 from utils.helpers import show_images
+from preprocess import preprocess_img
+
+def thresholding_RBC(preprocessed_img, lower_red1=np.array([0, 10, 45]), upper_red1= np.array([10, 255, 255]),
+                     lower_red2=np.array([160, 10, 45]), upper_red2= np.array([179, 255, 255])):
+   # pass preprocessed image to this function 
+   # it returns a binary mask for RBCs based on HSV thresholding
+   hsv_processed_img = cv2.cvtColor(preprocessed_img, cv2.COLOR_RGB2HSV)
+      
+   # the default values for thresholding ranges for the three channels H, S, V are tried and tested on image 00003 
+
+   mask1 = cv2.inRange(hsv_processed_img, lower_red1, upper_red1)
+   mask2 = cv2.inRange(hsv_processed_img, lower_red2, upper_red2)
+
+   RBC_mask = mask1 + mask2
+   return RBC_mask
+
+def segment_RBC(img):
+    pass
+
+
+img = cv2.imread('../data/input/JPEGImages/BloodImage_00003.jpg')
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+preprocessed_img = preprocess_img(img)
+thresholding_RBC_mask = thresholding_RBC(preprocessed_img)
+
+
+
+
+show_images([img_rgb, preprocessed_img, thresholding_RBC_mask], ['Original Image', 'Preprocessed Image', 'thresholding_RBC_mask'], [None, None, 'gray'])
+

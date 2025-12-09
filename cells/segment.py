943731983763db8fc2858a1d67e6fb312f22826d
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2025-12-09 23:05:16 +0200

    adabting thresholding - better results till now for the RBC

--- cells/segment.py
+++ cells/segment.py
@@ -51,14 +51,11 @@ import numpy as np
 from skimage import exposure
 from matplotlib import pyplot as plt
 from scipy import ndimage
-from skimage.feature import peak_local_max
 import os
 from utils.helpers import show_images
 from preprocess import preprocess_img
-
-
 from skimage.filters import frangi
-from skimage.color import rgb2gray
+
 
 
 def get_hessian_response(img):
@@ -66,14 +63,15 @@ def get_hessian_response(img):
 
     fr = frangi(
         gray,
-        sigmas=range(2, 6),   
-        alpha=0.5,
-        beta=0.9,
-        gamma=15
+        sigmas=range(1, 6),   
+        alpha=0.2,
+        beta=0.2,
+        gamma=5
     )
 
     fr = cv2.normalize(fr, None, 0, 255, cv2.NORM_MINMAX)
     fr = np.uint8(fr)
+   #  fr = cv2.equalizeHist(fr)
     return fr
 
 
@@ -98,69 +96,51 @@ def segment_RBC(img):
 
 
 # this part is for test till the work is done i put it here if you want to see how it is working, the flow i mean, delete it if you want
-img = cv2.imread('../data/input/JPEGImages/BloodImage_00002.jpg')
+img = cv2.imread('../data/input/JPEGImages/BloodImage_00020.jpg')
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 preprocessed_img = preprocess_img(img)
 thresholding_RBC_mask = thresholding_RBC(preprocessed_img)
 
 kernel_opening = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 img_opening = cv2.morphologyEx(thresholding_RBC_mask, cv2.MORPH_OPEN, kernel_opening) #for removing small noise
-
-# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-# dilated = cv2.dilate(img_opening, kernel, iterations=1)
-
-
+# kernel_dilation = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
+# img_dilation =  cv2.morphologyEx(img_opening, cv2.MORPH_DILATE , kernel_dilation)
 kernel_closing = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
 img_closing = cv2.morphologyEx(img_opening, cv2.MORPH_CLOSE, kernel_closing) #for closing small holes in RBCs
 
 
-# hessian_resp = get_hessian_response(preprocessed_img)
-# hessian_on_mask = cv2.bitwise_and(hessian_resp, hessian_resp, mask=img_closing)
 
-
-# Distance transform
 distance = cv2.distanceTransform(img_closing, cv2.DIST_L2, 5)
-# distance = distance * (1 + (hessian_on_mask / 255.0)) 
 
+_, labels = cv2.connectedComponents(img_closing)
 
-# Sure foreground (local maxima)
-_, sure_fg = cv2.threshold(distance, 0.2* distance.max(), 255, 0)
-sure_fg = np.uint8(sure_fg)
+sure_fg = np.zeros_like(distance, dtype=np.uint8)
+for i in range(1, labels.max()+1):
+    component = distance * (labels == i)  # extract distance values of this RBC
+    t = 0.3 * component.max()           # threshold for this RBC
+    sure_fg[component > t] = 255
 
-
-
-# Sure background
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
 sure_bg = cv2.dilate(img_closing, kernel, iterations=3)
 
-
-
-# Unknown region
 unknown = cv2.subtract(sure_bg, sure_fg)
 
-
-# Marker labelling
 _, markers = cv2.connectedComponents(sure_fg)
 markers = markers + 1
 markers[unknown == 255] = 0
 
-kernel_grad = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-grad = cv2.morphologyEx(img_closing, cv2.MORPH_GRADIENT, kernel_grad)
+
+
 hessian_resp = get_hessian_response(preprocessed_img)
-grad_hessian = cv2.addWeighted(grad.astype(np.float32), 1.0,
-                              hessian_resp.astype(np.float32), 1.0, 0)
-grad_hessian = np.uint8(grad_hessian)
+
+hessian_color  = cv2.cvtColor(hessian_resp, cv2.COLOR_GRAY2BGR)
 
 
-# Watershed
-# segmented = cv2.watershed(cv2.cvtColor(preprocessed_img, cv2.COLOR_RGB2BGR), markers)
 
-grad_hessian_color = cv2.cvtColor(grad_hessian, cv2.COLOR_GRAY2BGR)
-
-segmented = cv2.watershed(grad_hessian_color, markers)
-
+segmented = cv2.watershed(hessian_color, markers)
 result = preprocessed_img.copy()
-result[segmented == -1] = [255, 0, 0]   # red boundaries
+result[segmented == -1] = [255, 0, 0]
+
 
 distance_norm = cv2.normalize(distance, None, 0, 255, cv2.NORM_MINMAX)
 distance_norm = np.uint8(distance_norm)
@@ -168,13 +148,15 @@ sure_fg_vis = sure_fg.copy()
 sure_bg_vis = sure_bg.copy()
 
 
-
 show_images(
     [distance_norm, sure_fg_vis, sure_bg_vis, unknown, hessian_resp],
     ['Distance Transform', 'Sure FG (markers)', 'Sure BG', 'Unknown', 'hessian_resp'],
     ['gray', 'gray', 'gray', 'gray', 'gray']
 )
-show_images([segmented, result, hessian_resp, img_rgb, preprocessed_img], ['segmented', 'result', 'hessian_resp', 'img_rgb', 'preprocessed_img'], [None, 'gray', 'gray', None, None])
+
+show_images([segmented, result, hessian_resp, img_rgb, preprocessed_img], 
+            ['segmented', 'result', 'hessian_resp', 'img_rgb', 'preprocessed_img'], 
+            [None, 'gray', 'gray', None, None])
 
 
 show_images([img_rgb, preprocessed_img, thresholding_RBC_mask, img_opening, img_closing], 

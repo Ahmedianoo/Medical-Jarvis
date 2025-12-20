8b0e63af89d8ceb47377ee04b4aac90967b33327
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2025-12-20 13:31:38 +0200

    detect motion main logic and function is done

--- gestures/recognize.py
+++ gestures/recognize.py
@@ -32,6 +32,7 @@ import numpy as np
 def is_hand(features, min_area=45000, max_area=90000, min_circularity=0.08, max_circularity=0.75):
     """
         returns True if a valid hand is detected
+        the hand should be close enough to the laptop camera, but also not so close so that it takes the whole frame
     """
 
 
@@ -39,6 +40,10 @@ def is_hand(features, min_area=45000, max_area=90000, min_circularity=0.08, max_
 
 
 def get_static_gesture(features):
+    """
+        before calling this funtction, is_hand function must be called first to make sure if this is a hand or not
+        by doing some extra if conditions and adjusting the ranges, more static gestures could be detected
+    """
     v_shapes = features['convexity_defect']
     circularity = features['circularity']
     area = features['area']
@@ -52,4 +57,47 @@ def get_static_gesture(features):
         return 'POINT'
     return 'UNKOWN'
     
+
+def detect_motion(x1, y1, x2, y2, start, end, min_distance=400, dominant_axis_ration = 2):
+    """
+        this function will take an initial and final centroid, this delay of frames will be handled by the top level
+        also it will get the static hand gesture at the beginning and end of the motion, this also should be handled by the end of level
+        and the distance threshold to consider it as motion
+
+        and it will return the motion direction along with the type of this motion,
+            if the motion started with palm and ended with palm to the right, it is different from doing the motion in the same direction but with a fist
+
+        also this function does not handle the cursor tracking, this is handles by the top level, were we have the cntroids of the hand and know the gesture 
+
+        the caling of this function with the right parameters is the responsibilty of the top level   
+    """
+    dx = x1 - x2
+    dy = y1 - y2
+    distance = np.sqrt(dx ** 2 + dy ** 2)
+
+    motion = "UNKNOWN_MOTION"
+    direction = "UNKNOWN_DIRECTION"
+    if distance >= min_distance:
+        if start == 'PALM' and end == 'PALM':
+            motion = "PALM_PALM"
+            
+        if start == 'PALM' and end == 'FIST':
+            motion = "PALM_FIST"
+
+        if start == 'FIST' and end == 'PALM':
+            motion = "FIST_PALM"
+
+        if start == 'FIST' and end == 'FIST':
+            motion = "FIST_FIST"      
+
+        # we have to make sure it is not a random motion, it has to be in x or y direction, diagonal direction is not allowed here
+        if abs(dx) > dominant_axis_ration * abs(dy):
+            direction = 'RIGHT' if dx > 0 else 'LEFT'
+        elif abs(dy) > dominant_axis_ration * abs(dx):     
+            direction = 'DOWN' if dy > 0 else 'UP'
+
+
+
+    return motion, direction   
+
 
